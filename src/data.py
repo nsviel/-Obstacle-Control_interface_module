2f444606ccfe3cc1aa53f3135aeb760d480e1d2f
nsviel <nathan.sanchizviel@pm.me>
2022-07-22 15:25:51 +0200

    commit

--- src/data.py
+++ src/data.py
@@ -7,11 +7,11 @@ from scheme import scheme_plot
 
 
 def process_lidar_1_data(data):
-    path = param_co.state_py["lidar_1"]["file"]
+    path = param_co.state_co["path"]["path_l1_file"]
     io.write_lidar_data(path, data)
     scheme_plot.update_plot_l1(len(data))
 
 def process_lidar_2_data(data):
-    path = param_co.state_py["lidar_2"]["file"]
+    path = param_co.state_co["path"]["path_l2_file"]
     io.write_lidar_data(path, data)
     scheme_plot.update_plot_l2(len(data))
